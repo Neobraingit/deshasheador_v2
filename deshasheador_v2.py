eaf6bbd328a91ccf801ef41b701fb9cfb711bd56
neobraingit <neobrain@protonmail.com>
2024-03-30 16:50:40 +0100

    Finalizado

--- deshasheador_v2.py
+++ deshasheador_v2.py
@@ -2,6 +2,7 @@
 import hashlib
 import argparse
 import os
+from termcolor import colored
 
 parser = argparse.ArgumentParser(description= 'Convertidor de hash en texto plano')
 parser.add_argument( '--hash', type= str, required = True, help='Debes introducir un hash y un diccionario¡¡')
@@ -16,15 +17,15 @@ def main ():
             word = word.strip()
             word_crack = hashlib.sha1(word.encode()).hexdigest()
             if parser.hash == word_crack:
-                print (f'La contraseña es: {word}')
+                print (colored(f'La contraseña es: {word}','red'))
                 break
             else:
-                print (f'La contraseña no es {word}')
+                print (colored(f'La contraseña no es {word}', 'green'))
 
 
 if __name__=='__main__':
     try:
         main()
     except KeyboardInterrupt:
-        print ('Saliendo...')
+        print (colored('Saliendo...','yellow'))
         exit()
